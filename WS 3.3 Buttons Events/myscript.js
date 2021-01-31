d9a3efef753e95fc11d525637b499683cc1acae8
Kevin Nicolas <71517904+kevinnclas@users.noreply.github.com>
2021-01-31 18:47:17 +0100

    Add files via upload

---
+++ WS 3.3 Buttons Events/myscript.js
@@ -0,0 +1,11 @@
+function changeBackground() {
+    document.body.style.background = 'grey';
+}
+
+function deleteFirst() {
+    document.getElementById("button1").remove();
+}
+
+function allUpercase() {
+    document.getElementById("allButtons").innerHTML = document.getElementById("allButtons").innerHTML.toUpperCase();
+}
